1db042cc34c2749800a7fa0e4e2de2a872042c6d
unknown <ndayikengurukiyeinnocent95@gmail.com>
2022-09-06 17:45:27 +0200

    Gestions des droits et profils

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -1,6 +1,7 @@
 const verificationModel = require("../models/verificationModel")
 const Validation = require("../class/Validation");
 const { query } = require("../function/db");
+const { response } = require("express");
 const findByIdrequerant = async (req, res) => {
 
     try {
@@ -8,32 +9,83 @@ const findByIdrequerant = async (req, res) => {
 
         var requerantRDV = (await verificationModel.findByIdC(cq_id))[0];
         const requerant_Id = (await query("SELECT REQUERANT_ID FROM requerant WHERE TEMPO_REQUERANT_ID=?", [cq_id]))[0]
-        //const REQUERANT_ID=requerant_Id.REQUERANT_ID
+
         // console.log(requerant_Id)
         if (requerantRDV) {
+            const droit = (await verificationModel.checkdroit("USER_ID", req.userId))[0];
+            console.log(droit)
+            if (requerantRDV.PROVENANCE == 2 && requerantRDV.TRAITE==0) {
+
+                if (droit.GESTION_RENDEZ_VOUS != 1 || droit.USER_PROFILE_ID != 5) {
+                    return res.status(401).json({
+
+
+                        message: "Vous n'avez pas de  droit pour scanner ce certificat du voyageur entrant",
+                        requerantRDV,
+                        requerant_Id
+
+                    })
+                }
+
+
+            }
+            else if (requerantRDV.PROVENANCE == 1 && requerantRDV.TRAITE==0) {
+                if (droit.GESTION_RENDEZ_VOUS != 1 || droit.POINT_ENTREE_ID != 20) {
+                    return res.status(401).json({
+
+                        message: "Vous n'avez pas  droit pour scanner ce certificat du voyageur Sortant",
+                        requerantRDV,
+                        requerant_Id
+
+                    })
+
+
+                }
+
+            }
+
             const traite = (await verificationModel.findStatut("TEMPO_REQUERANT_ID", cq_id))[0];
 
             if (traite.TRAITE == 1) {
+
                 const Requerant_statut = (await verificationModel.findStatut_requerant("TEMPO_REQUERANT_ID", cq_id))[0];
 
                 if (Requerant_statut && Requerant_statut.REQUERANT_STATUT_ID == 3) {
+                    if (droit.GESTION_FILS_ATTENTE_LABO != 1) {
+                        return res.status(401).json({
+
+                            message: "Vous n'avez de  droit pour donner les résultats",
+                            requerantRDV,
+                            requerant_Id
+
+                        })
+                    }
                     return res.status(200).json
                         ({
                             success: true,
-                            messageResultat: "Atteinte des resultats",
+                            messageResultat: "Atteinte des résultats",
                             // token,
                             requerantRDV,
                             requerant_Id
                         })
                 }
+
                 const Requerant_statut_Id = (await verificationModel.find_Requerant_statut_Id("TEMPO_REQUERANT_ID", cq_id))[0];
                 //console.log(Requerant_statut_Id)
                 if (Requerant_statut_Id && Requerant_statut_Id.REQUERANT_STATUT_ID != 3 && Requerant_statut_Id.EST_GENERE == 0) {
+                    if (droit.VALIDATION_RESULTAT != 1) {
+                        return res.status(401).json({
 
+                            message: "Vous n'avez de  droit de validation des résultats",
+                            requerantRDV,
+                            requerant_Id
+
+                        })
+                    }
                     return res.status(200).json
                         ({
                             success: true,
-                            messageValidation: "Validation des resultats",
+                            messageValidation: "Validation des résultats",
                             // token,
                             requerantRDV,
                             requerant_Id
@@ -45,7 +97,7 @@ const findByIdrequerant = async (req, res) => {
                             success: true,
                             requerantRDV,
                             requerant_Id,
-                            message: "Le requerant n'a pas de droit a  la validation",
+                            message: "Le requerant n'a pas de droit   de validation",
 
                         })
                 }
@@ -58,7 +110,7 @@ const findByIdrequerant = async (req, res) => {
                     res.status(200).json
                         ({
                             success: true,
-                            message: "Payement en ligne ",
+                            // message: "Payement en ligne ",
                             messageTraite0: "Traite0",
                             // token,
                             requerantRDV,
@@ -70,7 +122,7 @@ const findByIdrequerant = async (req, res) => {
                     res.status(200).json
                         ({
                             success: true,
-                            message: "Payement Par banque ",
+                            // message: "Payement Par banque ",
                             requerantRDV,
                             payement,
                             messageTraite0: "Traite0",
--- models/verificationModel.js
+++ models/verificationModel.js
@@ -1,107 +1,124 @@
 const { query } = require("../function/db");
 const findByIdC = async (value) => {
-        try {
-
-          var sqlQuery = `SELECT tempo.TEMPO_REQUERANT_ID, tempo.NOM,tempo.DATE_VOYAGE,tempo.PRENOM ,tempo.TELEPHONE, tempo.DATE_NAISSANCE ,tempo.DATE_RENDEVOUS, tempo.EMAIL,tempo.NUMERO_DOCUMENT,tempo.LIEU_DE_NAISSANCE, tempo.ADRESSE_RESIDENCE, tempo.AEROPORT_EMBARQUEMA, tempo.DATE_INSERTION,labo_resultat_test.DATE_RESULTAT,vl_voyageur_documents.DOCUMENT_DESCR, countries.CommonName, requerant.DATE_PRELEVEMENT,requerant.REQUERANT_STATUT_ID,structures.STRUCTURE_NOM , labo_resultat_test.USER_ID,labo_resultat_test.RESULTAT_ID,requerant_changement_statut.USER_VALIDER_RESULTAT,requerant_changement_statut.DATE_VALIDATION_RESULTAT,requerant_changement_statut.DATE_TELECHARGEMENT_CERTIFICAT,users.USER_FNAME,users.USER_LNAME,requerant_tracking_gps.PATH_BORDEREAU,requerant_tracking_gps.PATH_PASSEPORT FROM tempo_requerant tempo LEFT JOIN vl_voyageur_documents ON vl_voyageur_documents.DOCUMENT_ID=tempo.DOCUMENT_ID LEFT JOIN countries ON countries.COUNTRY_ID=tempo.NATIONALITE_ID LEFT JOIN requerant ON requerant.TEMPO_REQUERANT_ID=tempo.TEMPO_REQUERANT_ID  LEFT JOIN structures ON structures.STRUCTURE_ID=tempo.STRUCTURE_ID LEFT JOIN  labo_resultat_test ON  labo_resultat_test.REQUERANT_ID=requerant.REQUERANT_ID LEFT JOIN requerant_changement_statut ON requerant.REQUERANT_ID=requerant_changement_statut.REQUERANT_ID LEFT JOIN users ON users.USER_ID=requerant_changement_statut.USER_ID LEFT JOIN requerant_tracking_gps ON requerant_tracking_gps.REQUERANT_ID=requerant.REQUERANT_ID `;
-          if (value.length > 10) {
-            sqlQuery += ` WHERE md5(tempo.TEMPO_REQUERANT_ID)=? `
-          } else {
-            sqlQuery += ` WHERE tempo.TEMPO_REQUERANT_ID = ? `
-          }
-          return query(sqlQuery, [value]);
-
-        }
-        catch (error) {
-          throw error
-        }
-
-}
-const findStatut= async  (column, value) => {
   try {
-    
-      var sqlQuery=`SELECT t.TRAITE FROM tempo_requerant t WHERE  ${column}= ?`
-      return query(sqlQuery, [value]);
-      
+
+    var sqlQuery = `SELECT tempo.TEMPO_REQUERANT_ID, tempo.NOM,tempo.TRAITE,tempo.DATE_VOYAGE,tempo.PRENOM ,tempo.TELEPHONE, tempo.DATE_NAISSANCE ,tempo.DATE_RENDEVOUS, tempo.EMAIL,tempo.NUMERO_DOCUMENT,tempo.LIEU_DE_NAISSANCE, tempo.ADRESSE_RESIDENCE, tempo.AEROPORT_EMBARQUEMA,tempo.PROVENANCE, tempo.DATE_INSERTION,labo_resultat_test.DATE_RESULTAT,vl_voyageur_documents.DOCUMENT_DESCR, countries.CommonName, requerant.DATE_PRELEVEMENT,requerant.REQUERANT_STATUT_ID,structures.STRUCTURE_NOM , labo_resultat_test.USER_ID,labo_resultat_test.RESULTAT_ID,requerant_changement_statut.USER_VALIDER_RESULTAT,requerant_changement_statut.DATE_VALIDATION_RESULTAT,requerant_changement_statut.DATE_TELECHARGEMENT_CERTIFICAT,users.USER_FNAME,users.USER_LNAME,requerant_tracking_gps.PATH_BORDEREAU,requerant_tracking_gps.PATH_PASSEPORT FROM tempo_requerant tempo LEFT JOIN vl_voyageur_documents ON vl_voyageur_documents.DOCUMENT_ID=tempo.DOCUMENT_ID LEFT JOIN countries ON countries.COUNTRY_ID=tempo.NATIONALITE_ID LEFT JOIN requerant ON requerant.TEMPO_REQUERANT_ID=tempo.TEMPO_REQUERANT_ID  LEFT JOIN structures ON structures.STRUCTURE_ID=tempo.STRUCTURE_ID LEFT JOIN  labo_resultat_test ON  labo_resultat_test.REQUERANT_ID=requerant.REQUERANT_ID LEFT JOIN requerant_changement_statut ON requerant.REQUERANT_ID=requerant_changement_statut.REQUERANT_ID LEFT JOIN users ON users.USER_ID=requerant_changement_statut.USER_ID LEFT JOIN requerant_tracking_gps ON requerant_tracking_gps.REQUERANT_ID=requerant.REQUERANT_ID `;
+    if (value.length > 10) {
+      sqlQuery += ` WHERE md5(tempo.TEMPO_REQUERANT_ID)=? `
+    } else {
+      sqlQuery += ` WHERE tempo.TEMPO_REQUERANT_ID = ? `
+    }
+    return query(sqlQuery, [value]);
+
   }
   catch (error) {
-      console.log(error)
-      throw error
+    throw error
   }
 
 }
-const findStatut_requerant= async  (column, value) =>{
+const findStatut = async (column, value) => {
   try {
-    
-      var sqlQuery=`SELECT REQUERANT_STATUT_ID FROM requerant WHERE  ${column}= ?`
-      return query(sqlQuery, [value]);
-      
-  }
-  catch (error) {
-      console.log(error)
-      throw error
-  }
 
-}
-const find_Requerant_statut_Id= async  (column, value) =>{
-  try{
-    
-      var sqlQuery=`SELECT REQUERANT_STATUT_ID,EST_GENERE FROM requerant WHERE   ${column}= ?`
-      return query(sqlQuery, [value]);
-      
+    var sqlQuery = `SELECT t.TRAITE FROM tempo_requerant t WHERE  ${column}= ?`
+    return query(sqlQuery, [value]);
+
   }
   catch (error) {
-      console.log(error)
-      throw error
+    console.log(error)
+    throw error
   }
 
 }
 
-const findPayement = async (cq_id) =>{
-        try {
 
-          var sqlQuery = `SELECT EMAIL_PAYE,MONTANT,DATE_INSERT_PAYEMENT,COMPTE_SIBLE,CARTE_TYPE,DEVISE FROM rdv_payement LEFT JOIN  tempo_requerant t ON t.RDV_ID=rdv_payement.RDV_ID  WHERE 1`;
-          if (cq_id.length > 10) {
-            sqlQuery += ` AND md5(TEMPO_REQUERANT_ID) = ? `
-          } else {
-            sqlQuery += ` AND TEMPO_REQUERANT_ID = ? `
-          }
-          return query(sqlQuery, [cq_id]);
-        }
-        catch (error) {
-          throw error
-        }
+const checkdroit = async (column, value) => {
+  try {
+
+    var sqlQuery = `SELECT users.POINT_ENTREE_ID,users.USER_PROFILE_ID,users_profile.GESTION_RENDEZ_VOUS,users_profile.VALIDATION_RESULTAT,users_profile.GESTION_FILS_ATTENTE_LABO FROM users LEFT JOIN users_profile ON users.USER_PROFILE_ID=users_profile.USER_PROFILE_ID  WHERE  ${column}= ?`
+    return query(sqlQuery, [value]);
+
+  }
+  catch (error) {
+    console.log(error)
+    throw error
+  }
+
+}
+
+const findStatut_requerant = async (column, value) => {
+  try {
+
+    var sqlQuery = `SELECT REQUERANT_STATUT_ID FROM requerant WHERE  ${column}= ?`
+    return query(sqlQuery, [value]);
+
+  }
+  catch (error) {
+    console.log(error)
+    throw error
+  }
+
+}
+const find_Requerant_statut_Id = async (column, value) => {
+  try {
+
+    var sqlQuery = `SELECT REQUERANT_STATUT_ID,EST_GENERE FROM requerant WHERE   ${column}= ?`
+    return query(sqlQuery, [value]);
+
+  }
+  catch (error) {
+    console.log(error)
+    throw error
+  }
+
+}
+
+const findPayement = async (cq_id) => {
+  try {
+
+    var sqlQuery = `SELECT EMAIL_PAYE,MONTANT,DATE_INSERT_PAYEMENT,COMPTE_SIBLE,CARTE_TYPE,DEVISE FROM rdv_payement LEFT JOIN  tempo_requerant t ON t.RDV_ID=rdv_payement.RDV_ID  WHERE 1`;
+    if (cq_id.length > 10) {
+      sqlQuery += ` AND md5(TEMPO_REQUERANT_ID) = ? `
+    } else {
+      sqlQuery += ` AND TEMPO_REQUERANT_ID = ? `
+    }
+    return query(sqlQuery, [cq_id]);
+  }
+  catch (error) {
+    throw error
+  }
 }
 const findhistorique = async (RDV_ID) => {
-        var sqlQuery = 'SELECT ID_RDV FROM historique WHERE 1 '
-        if (cq_id.length > 10) {
-          sqlQuery += ` AND md5(ID_RDV) = ? `
-        } else {
-          sqlQuery += ` AND ID_RDV = ? `
-        }
-        return query(sqlQuery, [RDV_ID])
+  var sqlQuery = 'SELECT ID_RDV FROM historique WHERE 1 '
+  if (cq_id.length > 10) {
+    sqlQuery += ` AND md5(ID_RDV) = ? `
+  } else {
+    sqlQuery += ` AND ID_RDV = ? `
+  }
+  return query(sqlQuery, [RDV_ID])
 
 }
 
 const findInRequerant = async (cq_id) => {
-        var sqlQuery = 'SELECT * FROM tempo_requerant WHERE TRAITE=1 '
-        if (cq_id.length > 10) {
-          sqlQuery += ` AND md5(TEMPO_REQUERANT_ID) = ? `
-        } else {
-          sqlQuery += ` AND TEMPO_REQUERANT_ID = ? `
-        }
-        return query(sqlQuery, [cq_id])
+  var sqlQuery = 'SELECT * FROM tempo_requerant WHERE TRAITE=1 '
+  if (cq_id.length > 10) {
+    sqlQuery += ` AND md5(TEMPO_REQUERANT_ID) = ? `
+  } else {
+    sqlQuery += ` AND TEMPO_REQUERANT_ID = ? `
+  }
+  return query(sqlQuery, [cq_id])
 
 }
 
 
 
 module.exports = {
-      findByIdC,
-      findPayement,
-      findhistorique,
-      findInRequerant,
-      findStatut,
-      findStatut_requerant,
-      find_Requerant_statut_Id
+  findByIdC,
+  findPayement,
+  findhistorique,
+  findInRequerant,
+  findStatut,
+  findStatut_requerant,
+  find_Requerant_statut_Id,
+  checkdroit
 
 }
