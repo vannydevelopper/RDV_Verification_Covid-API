55f0a3f60bbafa014b0c617444f558beab275988
vanny <vdevelopper@gmail.com>
2022-09-13 10:05:15 +0200

    configuration de la base de donnees en local

--- middleware/bindUser.js
+++ middleware/bindUser.js
@@ -2,8 +2,10 @@ const jwt = require('jsonwebtoken')
 const bindUser = (req, res, next) =>{
     const bearer = req.headers.authorization
     const bearerToken = bearer && bearer.split(' ')[1]
+    console.log('bearer')
     if (bearerToken) {
         jwt.verify(bearerToken, 'fffffcccckkkkpppp',(error, user) =>{
+
             if (user){
                 req.userId = user.user
             }
